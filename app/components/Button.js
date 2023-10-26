da00da31cd29a01463de4ae36d9a6c370e0c503d
A-yu-sh <ayush.wd.dev@gmail.com>
2023-10-26 15:10:46 +0530

    auth implementation : SUCCESS

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,10 +1,27 @@
 "use client";
 import React from "react";
+import { useSession } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const SignInButton = () => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  if (status === "authenticated") {
+    return (
+      <div>
+        <p className="flex justify-center mt-5">Hello, {session.user.name}</p>
+        <button className="relative ml-[48%] mt-3" onClick={() => signOut()}>
+          sign Out
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="flex justify-center mt-5">
-      <button>sign In</button>
+      <button onClick={() => signIn("google")}>sign In</button>
     </div>
   );
 };
