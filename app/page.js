81a5b3996b55d9086ac0505c07adc0f762d38002
A-yu-sh <ayush.wd.dev@gmail.com>
2023-11-16 23:57:55 +0530

    updates

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ import { GET_DATA, GET_DATAA } from "./api/Products/route";
 export default async function Home() {
   return (
     <main>
-      <h1 className="flex justify-center text-4xl mt-5 font-mono">RBAC</h1>
+      <h1 className="flex justify-center text-4xl mt-5 font-mono">
+        RBAC_role based access control
+      </h1>
       <Navbar />
       <SignInButton />
     </main>
