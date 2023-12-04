08151573519413c81d3e25d17bfcc1e3b3833307
A-yu-sh <ayush.wd.dev@gmail.com>
2023-12-04 23:55:19 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <main>
       <h1 className="flex justify-center text-4xl mt-5 font-mono">
-        RBAC_role based access control(rba)
+        RBAC_role based access control(rbac)
       </h1>
       <Navbar />
       <SignInButton />
