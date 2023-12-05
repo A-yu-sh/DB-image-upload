81f33402b32d730aca4a4150f3283eafa43f0a81
A-yu-sh <ayush.wd.dev@gmail.com>
2023-12-05 23:47:12 +0530

    updates

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
+"use client";
 import Navbar from "./components/Navbar";
 import SignInButton from "./components/Button";
 import { GET_DATA, GET_DATAA } from "./api/Products/route";
+import { useState } from "react";
 
-export default async function Home() {
+export default function Home() {
   return (
     <main>
       <h1 className="flex justify-center text-4xl mt-5 font-mono">
-        RBAC_role based access control(rbac)
+        RBAC_role based access control(rba)
       </h1>
       <Navbar />
       <SignInButton />
