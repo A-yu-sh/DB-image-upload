eee24cb1545e1974e157e840689a0a19f6bf18be
A-yu-sh <ayush.wd.dev@gmail.com>
2023-12-06 22:55:57 +0530

    fixes

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="flex justify-center text-4xl mt-5 font-mono">
-        RBAC_role based access control(rba)
+        RBAC_role based access control(rbac)
       </h1>
       <Navbar />
       <SignInButton />
