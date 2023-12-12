79db7602fdba2b5511201f893714a879abb3d722
A-yu-sh <ayush.wd.dev@gmail.com>
2023-12-12 23:52:41 +0530

    updates

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 "use client";
 import Navbar from "./components/Navbar";
 import SignInButton from "./components/Button";
-import { GET_DATA, GET_DATAA } from "./api/Products/route";
-import { useState } from "react";
 
 export default function Home() {
   return (
