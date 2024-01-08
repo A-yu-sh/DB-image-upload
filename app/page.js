42eca725608bf460ceca366d7a12b14ae2c4ec86
A-yu-sh <ayush.wd.dev@gmail.com>
2024-01-08 23:20:18 +0530

    commit updates

--- app/page.js
+++ app/page.js
@@ -8,8 +8,6 @@ export default function Home() {
       <h1 className="flex justify-center text-4xl mt-5 font-mono">
         RBAC_role based access control(rbac)
       </h1>
-      <Navbar />
-      <SignInButton />
     </main>
   );
 }
