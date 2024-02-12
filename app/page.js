b8ade5601720957c355f197a0689ab3f65a8d8a5
A-yu-sh <ayush.wd.dev@gmail.com>
2024-02-12 23:33:28 +0530

    none

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="flex justify-center text-4xl mt-5 font-mono">
-        RBAC_role based access
+        RBAC_role based access control
       </h1>
     </main>
   );
